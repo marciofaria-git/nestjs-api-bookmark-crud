e6fa194b7b0dcd4ab0bece651930c1eba4fd9265
devmarciofaira <devmarciofaria@gmail.com>
2023-01-31 16:30:03 -0300

    add dto and pipe validation for the email and pass

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
+  signup(dto: AuthDto) {
     return { msg: 'I have signed up' };
   }
 
